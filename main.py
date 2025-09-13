996a1cd73be114be51a1c80bd281fe05452522c7
Moses Ayite <ayitemoses@gmail.com>
2025-09-13 07:38:15 -0400

    feat(ec2-i-c): ajout script de maj ec2 instance and python install

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ KEY_NAME = "vockey"
 SECURITY_GROUP_NAME = "default"
 REGION_NAME = "us-east-1"
 
+# ajout d'un script pour mise a jour et installation de python dans chaque instance
+user_data_script = '''#!/bin/bash
+yum update -y
+yum install -y python3 python3-pip
+'''
 
