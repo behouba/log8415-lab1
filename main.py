c20e057afde60d07307768059f66b40d432bea04
Moses Ayite <ayitemoses@gmail.com>
2025-09-13 07:34:47 -0400

    feat(ec2-i-c) ajout de variables d'environnement

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # import des bibliothèques nécessaires
 import boto3
 import botocore.exceptions
+
+# --- Paramètres communs avec les variables d'environnement ---
+AMI_ID = "ami-00ca32bbc84273381"
+KEY_NAME = "vockey"
+SECURITY_GROUP_NAME = "default"
+REGION_NAME = "us-east-1"
+
+
