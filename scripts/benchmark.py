67dc7474b5f512cce3ac41f379fc013133b3bbf6
Mehdi-Ilyes <mehdi-ilyes.haddoune@polymtl.ca>
2025-09-28 02:24:53 -0400

    feat : CLI argument parsing

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -5,7 +5,13 @@ import time
 import sys
 
 async def main():
-    pass
+    if len(sys.argv) < 2:
+        print("Usage: python benchmark.py <load_balancer_base_url>")
+        print("Example: python benchmark.py http://98.87.148.211")
+        sys.exit(1)
+
+    base_url = sys.argv[1]
+    num_requests = 1000
 
 if __name__ == "__main__":
     asyncio.run(main())
